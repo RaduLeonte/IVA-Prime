b981dde3c5497398ffb7e45b986744a80a8fffdd
RaduLeonte <cristian.radu.leonte@gmail.com>
2023-10-05 14:05:30 +0200

    added documentation

--- js/createPrimers.js
+++ js/createPrimers.js
@@ -1,4 +1,7 @@
 
+/**
+ * Global variables
+ */
 let primerConc = 100E-9; // M, primer concentration for melting temperatures
 let saltConc = 0.5; // M, primer concentration for melting temperatures
 
@@ -11,9 +14,18 @@ let operationNr = 1; // modification counter
 /**
  * Display the primer pair in the sidebar by appending <p>s
  * 
+ * primersType - time of modification (e.g. insertion, deletion etc)
+ * primersList - list of primers [homoFwd, tempFwd, homoRev, tempRev]
+ * textColor - color of text, usually white
+ * templateColor - background color of template part
+ * homoColor - background color of homologous part
+ * mutSeq - additional sequence for mutations
+ * 
  * TO DO:
  * - group <p>s into div
  * - save to dict so they can be used later to generate a protocol?
+ * - use a loop to create the two primers and change structure of primersList to nested list and maybe
+ * make a dict to combine with colors
  */
 function displayPrimers(primersType, primersList, textColor, templateColor, homoColor, mutSeq) {
     const sidebarContentDiv = document.querySelector('.sidebar-content'); // Select sidebar
@@ -31,16 +43,18 @@ function displayPrimers(primersType, primersList, textColor, templateColor, homo
     sidebarContentDiv.appendChild(p);
 
 
+
     // First Pair of primers
     const paragraph1 = document.createElement('p');
     paragraph1.style.wordWrap = 'break-word'; // Add CSS style for word wrapping
+
     // Create individual spans for the homologous and template regions of the primers  with different colors
     const span1a = document.createElement('span'); 
     span1a.style.color = textColor;
     span1a.style.backgroundColor = homoColor;
     span1a.style.fontWeight = 'bold';
     span1a.textContent = primersList[2];
-    // Create the second span with green text and bold
+
     const span1b = document.createElement('span');
     span1b.style.color = textColor;
     span1b.style.backgroundColor = templateColor;
@@ -50,6 +64,7 @@ function displayPrimers(primersType, primersList, textColor, templateColor, homo
     paragraph1.appendChild(span1a);
     paragraph1.appendChild(span1b);
 
+    // Calculate melting temperatures and add to the paragraph
     const spanTm1 = document.createElement('span');
     let tmTuple = [];
     if (primersList[2] !== "") {
@@ -58,10 +73,10 @@ function displayPrimers(primersType, primersList, textColor, templateColor, homo
     if (primersList[3] !== "") {
         tmTuple.push(get_tm(primersList[3], primerConc, saltConc).toFixed(2))
     }
-    spanTm1.textContent = `(${tmTuple.join(',')})`
+    spanTm1.textContent = ` (${tmTuple.join(',')})`
     paragraph1.appendChild(spanTm1);
 
-    // Create the second paragraph
+    // Second Pair of primers
     const paragraph2 = document.createElement('p');
     paragraph2.style.wordWrap = 'break-word'; // Add CSS style for word wrapping
     // Create the first span with red text and bold
@@ -101,11 +116,12 @@ function displayPrimers(primersType, primersList, textColor, templateColor, homo
     //spanTm2.textContent = " (" + get_tm(primersList[0], primerConc, saltConc).toFixed(1) + ", " + get_tm(primersList[1], primerConc, saltConc).toFixed(1) + ")";
     paragraph2.appendChild(spanTm2);
 
-    // Find the <p> with the id "primers-type"
 
     // Insert the new paragraphs after the <p> with id "primers-type"
     sidebarContentDiv.appendChild(paragraph2);
     sidebarContentDiv.appendChild(paragraph1);
+    
+    // Special case for subcloning which requires 4 primers
     if (primersType === "Subcloning") {
         const paragraph3 = document.createElement('p');
         const paragraph4 = document.createElement('p');
@@ -132,23 +148,63 @@ function displayPrimers(primersType, primersList, textColor, templateColor, homo
     }
 }
 
+
+/**
+ * Extends the primer from the starting position in the specified direction on the specified strand
+ * 
+ * Example:
+ *                                     startingPos  
+ *                                         |
+ *                          <--- backward  ▼  forward  --->
+ * fwdStrand  -> GGGGAAAAAAAATTTATATATGGGGAAAAAAAATTTATATATGGGGAAAAAAAATTTATATAT
+ * compStrand -> ATATATAAATTTTTTTTCCCCATATATAAATTTTTTTTCCCCATATATAAATTTTTTTTCCCC
+ *                          <--- forward   ▲  backward --->
+ * 
+ * 
+ * Backward extension on fwdStrand, ATG as the mutseq:
+ *                                     startingPos  
+ *                                         |
+ *                                         ▼  
+ * fwdStrand  -> GGGGAAAAAAAATTTATATATGGGGAAAAAAAATTTATATATGGGGAAAAAAAATTTATATAT
+ *                           <-------------
+ *                           TTTATATATGGATG
+ *                           ▲
+ *                           |
+ *                           Melting temperature exceeds targetTm here.
+ * 
+ * 
+ * startingPos - initial position
+ * targetStrand - strand where the extension takes place, "fwdStrand" or "compStrand"
+ * direction - either "forward" or "backward" (see above, directionality is flipped on the complementary strand)
+ * targetTm - the function extends the primer until the primer has a melting temperature above targetTm
+ * minLength - a minimum length to make sure the melting temperature calculator does not give NaN
+ * pNr - plasmid number, important when the second plasmid is imported for subcloning, 1 or 2
+ * mutSeq - sequence that will be inserted and will be the starting sequence for the homologous region of the forward primer
+ *  
+ * TO DO:
+ * - Clean up, some lines are way too long
+ * - Maybe change to always favour the primer with the higher temperature?
+ */
 function primerExtension(startingPos, targetStrand, direction, targetTm, minLength, pNr, mutSeq) {
     console.log("PE", startingPos, targetStrand, direction, targetTm, minLength, pNr, mutSeq)
-    let p_start_index = startingPos - 1;
-    let length = minLength;
+    
+    let p_start_index = startingPos - 1; // Convert sequence index to string index
+    let length = minLength; // initial value for length
 
+    // Set working strand
     let currStrand = targetStrand === 'fwdStrand' ? sequence : complementaryStrand;
+    // If we're working on the second plasmid, change working strand to that
     if (pNr === 2) {
         currStrand = targetStrand === 'fwdStrand' ? sequence2 : complementaryStrand2;
     }
 
+    // Check if we have a sequence to start with
     let accessory = ""
     if (mutSeq) {
         accessory = mutSeq;
     }
 
-    
-    //let prev_p = direction === 'forward' ? backbone.slice(p_start_index, p_start_index + length - 1): backbone.slice(p_start_index - length, p_start_index);
+    // Initialise previous primer based on target strand and direction and using the min length
     let prev_p = "";
     if (direction === "forward") {
         prev_p = targetStrand === 'fwdStrand' ? repeatingSlice(currStrand, p_start_index, p_start_index + length - 1) + accessory: repeatingSlice(currStrand, p_start_index - length, p_start_index);
@@ -156,12 +212,14 @@ function primerExtension(startingPos, targetStrand, direction, targetTm, minLeng
         prev_p = targetStrand === 'fwdStrand' ? repeatingSlice(currStrand, p_start_index - length + 1, p_start_index) + accessory: repeatingSlice(currStrand, p_start_index, p_start_index - length);
     }
     console.log("prev_p", targetStrand, direction, p_start_index, prev_p)
-    let prev_tm = get_tm(prev_p, primerConc, saltConc);
-    const maxIter = 50;
+    let prev_tm = get_tm(prev_p, primerConc, saltConc); // Get melting temperature of initial primer
+    
+    // Main loop for the extension of the primer
+    const maxIter = 50; // Maximum amount of iterations in case of errors
     let i = 0;
     while (i < maxIter) {
-        //let curr_p = direction === 'forward' ? backbone.slice(p_start_index, p_start_index + length): backbone.slice(p_start_index - length - 1, p_start_index);
-        let curr_p = "";
+        let curr_p = ""; // Reset current primer
+        // Set current primer using the current length then get its temperature
         if (direction === "forward") {
             curr_p = targetStrand === 'fwdStrand' ? repeatingSlice(currStrand, p_start_index, p_start_index + length) + accessory: repeatingSlice(currStrand, p_start_index - length - 1, p_start_index);
         } else {
@@ -169,6 +227,8 @@ function primerExtension(startingPos, targetStrand, direction, targetTm, minLeng
         }
         let curr_tm = get_tm(curr_p, primerConc, saltConc);
 
+        /** If the melting temperature of the current primer exceeds the target temperature, break the loop 
+         * and return either the current primer or the previous one, whichever is closest to the target temperature*/ 
         if (curr_tm >= targetTm) {
             if (Math.abs(curr_tm - targetTm) <= Math.abs(prev_tm - targetTm)) {
                 primer_fwd_tm = curr_tm;
@@ -180,25 +240,38 @@ function primerExtension(startingPos, targetStrand, direction, targetTm, minLeng
             break;
         }
 
+        // Adjusting loop variables
         prev_tm = curr_tm;
         prev_p = curr_p;
         length += 1;
         i++;
     }
+
+    // Primer needs to be flipped if it was not fromt he fwdStrand
     if (targetStrand !== "fwdStrand") {
         primer_fwd = primer_fwd.split('').reverse().join('');
     }
 
+    // Move the accessory (mutSeq) from the front to the back for compStrand
     if (direction !== "forward") {
-        //primer_fwd = primer_fwd.split('').reverse().join('');
         primer_fwd = primer_fwd.replace(accessory, "") + accessory;
     }
 
-    // console.log(primer_fwd, primer_fwd_tm)
     return primer_fwd;
 }
 
+
+/**
+ * Function that recursively creates every possible DNA sequence for the specificied amino acid sequence if
+ * the length is under 10 amino acids, in which case it only returns the first DNA sequence generated.
+ *  
+ * TO DO:
+ * - Make the algorithm not have an O(n!), maybe cache the melting temperatures of the codons
+ * - Add an option to optimize for a specific organism
+ * - Maybe optimize GC content?
+ */
 function generateDNASequences(aminoAcidSequence) {
+    // Possible codons resulting in each amino acid
     const codons = {
       A: ['GCT', 'GCC', 'GCA', 'GCG'],
       R: ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
@@ -223,25 +296,52 @@ function generateDNASequences(aminoAcidSequence) {
       X: ['TAA', 'TAG', 'TGA']
     };
   
-    const results = [];
+    const results = []; // Initialize empty list for generated sequences
   
+    /**
+     * The function looks at the amino acid in the amino acid sequence specified by the index and calls itself for each possible codon
+     * that corresponds to the current amino acid.
+     * 
+     * Example for WYD:
+     * 
+     *                                           W
+     *                                           |
+     *                                           |
+     *                                         [TGG]
+     *                                           |
+     *                                           |
+     *                                          WY
+     *                                           |
+     *                        --------------------------------------	
+     *                        |                                    |
+     *                 [TGG][TAT]                           [TGG][TAC]
+     *                        |                                    | 
+     *                       WYD                                 WYD
+     *                         |                                   |
+     *                -------------------	              -------------------
+     *                |                 |                 |                 |
+     *    [TGG][TAT][GAT]   [TGG][TAT][GAC]   [TGG][TAC][GAT]   [TGG][TAT][GAC] } -> Results
+     */
     function generateSequences(aminoAcidSeq, index, currentSeq) {
-      if (index === aminoAcidSeq.length) {
-        results.push(currentSeq);
-        return;
-      }
-  
-      const aminoAcid = aminoAcidSeq[index];
-      const possibleCodons = codons[aminoAcid];
-  
-      for (let codon of possibleCodons) {
-        generateSequences(aminoAcidSeq, index + 1, currentSeq + codon);
-      }
+        // Check if we've reached the end of the recursive tree
+        if (index === aminoAcidSeq.length) {
+            results.push(currentSeq); // Save progress by pushing to results
+            return;
+        }
+
+        const aminoAcid = aminoAcidSeq[index]; // Current amino acid
+        const possibleCodons = codons[aminoAcid]; // Return list of possible codonds for current amino acid
+
+        // For each possible codon, append the codon to the current sequence then branch the tree with an incremented index
+        for (let codon of possibleCodons) {
+            generateSequences(aminoAcidSeq, index + 1, currentSeq + codon);
+        }
     }
   
+    // If the sequence is less than 10 acids, generate all possible DNA sequences
     if (aminoAcidSequence.length < 10) {
         generateSequences(aminoAcidSequence, 0, '');
-    } else {
+    } else { // Else only choose the first codon for each acid
         let basicSeq = "";
         for (let aminoAcid of aminoAcidSequence) {
             const possibleCodons = codons[aminoAcid];
@@ -250,7 +350,6 @@ function generateDNASequences(aminoAcidSequence) {
           }
         results.push(basicSeq);
     }
-
   
     return results;
   }
