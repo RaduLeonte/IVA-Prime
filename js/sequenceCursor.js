2349c08b4b15a231bdd82068c030e7f697cd46ed
RaduLeonte <cristian.radu.leonte@gmail.com>
2023-10-11 17:41:35 +0200

    added documentation

--- js/sequenceCursor.js
+++ js/sequenceCursor.js
@@ -1,76 +1,87 @@
+/**
+ * Enables a cursor hover hint displaying the cursor position.
+ */
 function addCellBorderOnHover(tableId, containerId, pNr) {
-    const sequenceGridTable = document.getElementById(tableId);
-    const fileContentContainer = document.getElementById(containerId);
-  
-    if (!sequenceGridTable) {
-      // Table doesn't exist yet, observe the DOM for changes
-      const observer = new MutationObserver(function(mutationsList) {
-        for (const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-            // Check if the table is added
-            const addedNodes = Array.from(mutation.addedNodes);
-            const isTableAdded = addedNodes.some(node => node.id === tableId);
-  
-            if (isTableAdded) {
-              observer.disconnect(); // Stop observing DOM changes
-              addCellBorderOnHover(tableId, containerId, pNr); // Call the function again now that the table exists
-              break;
-            }
+  // Select the grid table of interest
+  const sequenceGridTable = document.getElementById(tableId);
+
+  // If the table doesn't exist yet, wait for it
+  if (!sequenceGridTable) {
+    // Define observer
+    const observer = new MutationObserver(function(mutationsList) {
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          // Check if the table is added
+          const addedNodes = Array.from(mutation.addedNodes);
+          const isTableAdded = addedNodes.some(node => node.id === tableId);
+
+          if (isTableAdded) {
+            observer.disconnect(); // Stop observing DOM changes
+            addCellBorderOnHover(tableId, containerId, pNr); // Call the function again now that the table exists
+            break;
           }
-        }
-      });
-  
-      observer.observe(document.documentElement, { childList: true, subtree: true });
-      return; // Exit the function for now, it will be called again when the table is added
-    }
-  
-    let previousCell = null;
-  
-    sequenceGridTable.addEventListener('mousemove', function(event) {
-      const cell = event.target.closest('td'); // Get the hovered cell
-      if (cell && cell !== previousCell) {
-        // Reset previous cell border styles
-        if (previousCell) {
-          previousCell.style.borderLeft = '';
-          previousCell.style.borderRight = '';
-        }
-  
-        previousCell = cell;
-      }
-  
-      if (previousCell) {
-        const cellRect = previousCell.getBoundingClientRect();
-        const cellWidth = cellRect.width;
-        const cellLeft = cellRect.left;
-        const cellRight = cellRect.right;
-        const cursorX = event.clientX;
-  
-        // Calculate the distance from the cursor to the left and right sides of the cell
-        const distanceToLeft = cursorX - cellLeft;
-        const distanceToRight = cellRight - cursorX;
-  
-        // Apply border style to the side closest to the cursor
-        if (distanceToLeft < distanceToRight) {
-          previousCell.style.borderLeft = '2px solid red';
-          previousCell.style.borderRight = '';
-        } else {
-          previousCell.style.borderRight = '2px solid red';
-          previousCell.style.borderLeft = '';
         }
       }
     });
-  
-    sequenceGridTable.addEventListener('mouseleave', function(event) {
-      // Reset cell border styles when leaving the table
+    // Run observer
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+    return;
+  }
+
+  let previousCell = null; // Previously hovered cell
+  // Event listener for mouse movements
+  sequenceGridTable.addEventListener('mousemove', function(event) {
+    // Get the closest cell to the cursor
+    const cell = event.target.closest('td');
+    
+    // If the cell exists and its not the same as the previous one, i.e. the cursor has moved to a new one
+    if (cell && cell !== previousCell) {
+      // Reset previous cell border styles
       if (previousCell) {
         previousCell.style.borderLeft = '';
         previousCell.style.borderRight = '';
-        previousCell = null;
       }
-    });
+      // Update the cell tracker
+      previousCell = cell;
+    }
+
+    // If the cursor has changed cells
+    if (previousCell) {
+      // Find the area of the cell
+      const cellRect = previousCell.getBoundingClientRect();
+      // Find the borders of the cell
+      const cellLeft = cellRect.left;
+      const cellRight = cellRect.right;
+      // Get the cursors' x coordinate
+      const cursorX = event.clientX;
+
+      // Calculate the distance from the cursor to the left and right sides of the cell
+      const distanceToLeft = cursorX - cellLeft;
+      const distanceToRight = cellRight - cursorX;
+
+      // Apply border style to the side closest to the cursor
+      if (distanceToLeft < distanceToRight) {
+        previousCell.style.borderLeft = '2px solid red';
+        previousCell.style.borderRight = '';
+      } else {
+        previousCell.style.borderRight = '2px solid red';
+        previousCell.style.borderLeft = '';
+      }
+    }
+  });
+
+  // If the mouse leaves the table, deselect
+  sequenceGridTable.addEventListener('mouseleave', function(event) {
+    // Reset cell border styles when leaving the table
+    if (previousCell) {
+      previousCell.style.borderLeft = '';
+      previousCell.style.borderRight = '';
+      previousCell = null;
+    }
+  });
 }
 
 
-// Call the function to add the cell border on hover effect to your sequence grid table
+// Enable the cursor hover hint
 addCellBorderOnHover('sequence-grid', 'file-content', 1);
 addCellBorderOnHover('sequence-grid2', 'file-content2', 2);
