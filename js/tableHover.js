bd6db289cd4a33e17ca8c0373ab647b19e766485
RaduLeonte <cristian.radu.leonte@gmail.com>
2023-10-11 17:52:34 +0200

    added documentation

--- js/tableHover.js
+++ js/tableHover.js
@@ -1,52 +1,58 @@
+/**
+ * Enables the cursor hvoer hint displaying the position in the sequence and table coordinates.
+ * 
+ * TO DO:
+ * - dont create the popup on every mousement
+ * - dont delete the popup just hide it
+ */
 function addHoverPopupToTable(tableId, pNr) {
+  // Select table
   const table = document.getElementById(tableId);
 
+  // Add the hover listener
   table.addEventListener('mouseover', function(event) {
+    // Check if we're hovering over a cell
     if (event.target.tagName === 'TD') {
+      // Get cell coordinates
       const row = event.target.parentNode;
       const rowIndex = row.rowIndex;
       const cellIndex = event.target.cellIndex;
 
+      // Create the hover popup
       const popup = document.createElement('div');
       popup.className = 'hover-popup';
+      // Insert the popup text based on the hovered plasmid
       if (pNr === 1) {
         popup.textContent = basePosition !== -1 ? basePosition + " (" + rowIndex + ", " + cellIndex + ")" : "";
       } else {
         popup.textContent = basePosition2 !== -1 ? basePosition2 + " (" + rowIndex + ", " + cellIndex + ")" : "";
       }
       
-
+      // Append the popup and position it accordingly
       document.body.appendChild(popup);
       positionPopup(popup, event.clientX, event.clientY);
     }
   });
 
-  table.addEventListener('mouseout', function() {
-    const popup = document.querySelector('.hover-popup');
-    if (popup) {
-      document.body.removeChild(popup);
-    }
-    if (pNr === 1) {
-      basePosition = -1;
-    } else {
-      basePosition2 = -1;
-    }
-    
-  });
 
+  /**
+   * Tracks the cursor position and updates the position of the hover popup.
+   */
   table.addEventListener('mousemove', function(event) {
-    if (event.target.tagName === 'TD') {
+    if (event.target.tagName === 'TD') { // Check to see if we're hovering over a cell
+      // Find cell dimensions
       const cellRect = event.target.getBoundingClientRect();
-      const cursorOffset = event.clientX - cellRect.left;
+      const cursorOffset = event.clientX - cellRect.left; // Cursor position inside the cell
       const cellWidth = cellRect.width;
 
-      if (cursorOffset < cellWidth / 2) {
+      // Check which side of the cell we're closer to
+      if (cursorOffset < cellWidth / 2) { // Left side
         if (pNr === 1) {
           basePosition = ((event.target.parentNode.rowIndex - event.target.parentNode.rowIndex % gridStructure.length) / gridStructure.length) * gridWidth + event.target.cellIndex + 1;
         } else {
           basePosition2 = ((event.target.parentNode.rowIndex - event.target.parentNode.rowIndex % gridStructure2.length) / gridStructure2.length) * gridWidth + event.target.cellIndex + 1;
         }
-      } else {
+      } else { // Right side
         if (pNr === 1) {
           basePosition = ((event.target.parentNode.rowIndex - event.target.parentNode.rowIndex % gridStructure.length) / gridStructure.length) * gridWidth + event.target.cellIndex + 2;
         } else {
@@ -54,6 +60,7 @@ function addHoverPopupToTable(tableId, pNr) {
         }
       }
 
+      // Update the text content and mvoe the popup into position
       const popup = document.querySelector('.hover-popup');
       if (popup) {
         if (pNr === 1) {
@@ -66,9 +73,29 @@ function addHoverPopupToTable(tableId, pNr) {
       }
     }
   });
+
+
+  // Disable the popup if leaving the table and reset the hovering position tracker
+  table.addEventListener('mouseout', function() {
+    const popup = document.querySelector('.hover-popup');
+    
+    if (popup) {
+      document.body.removeChild(popup);
+    }
+
+    if (pNr === 1) {
+      basePosition = -1;
+    } else {
+      basePosition2 = -1;
+    }
+  });
+
 }
 
 
+/**
+ * Changes the position of the popup to the target coordinates.
+ */
 function positionPopup(popup, clientX, clientY) {
   const popupWidth = popup.offsetWidth;
   const popupHeight = popup.offsetHeight;
@@ -88,6 +115,9 @@ function positionPopup(popup, clientX, clientY) {
 }
 
 
+/**
+ * Wait for tables to exist then return.
+ */
 function waitForTableToExist(tableId, callback) {
   const checkTableExistence = setInterval(function() {
     const table = document.getElementById(tableId);
@@ -99,6 +129,7 @@ function waitForTableToExist(tableId, callback) {
 }
 
 
+// Listeners waiting for the tables to exist before enabling the cursor hover hint
 waitForTableToExist('sequence-grid', function() {
   addHoverPopupToTable('sequence-grid', 1);
 });
