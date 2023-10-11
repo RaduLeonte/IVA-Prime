92dc6c386cca6f25251b3dc9c9674628736f2db7
RaduLeonte <cristian.radu.leonte@gmail.com>
2023-10-11 17:35:06 +0200

    added documentation

--- js/sequenceContextMenu.js
+++ js/sequenceContextMenu.js
@@ -1,128 +1,125 @@
-function updateMenuItems() {
-  const insertionMenuItem = document.getElementById('insertion');
-  const deletionMenuItem = document.getElementById('deletion');
-  const mutationMenuItem = document.getElementById('mutation');
-  const subcloningMenuItem = document.getElementById('subcloning');
-  const beginTranslationMenuItem = document.getElementById('begin-translation');
-
-  // Example logic for updating menu item states based on selectedText
-  console.log(selectedText)
-  if (selectedText) {
-    insertionMenuItem.classList.add('disabled');
-    beginTranslationMenuItem.classList.add('disabled');
-
-    deletionMenuItem.classList.remove('disabled');
-    mutationMenuItem.classList.remove('disabled');
-  } else {
-    insertionMenuItem.classList.remove('disabled');
-    beginTranslationMenuItem.classList.remove('disabled');
-
-    deletionMenuItem.classList.add('disabled');
-    mutationMenuItem.classList.add('disabled');
-  }
-
-  if (selectedText && secondPlasmidIported) {
-    subcloningMenuItem.classList.remove('disabled');
-  } else {
-    subcloningMenuItem.classList.add('disabled');
-  }
-}
-
-
+/**
+ *  Custom right click context menu.
+ */
+// Wait for document to load
 document.addEventListener('DOMContentLoaded', function () {
+  // Create the menu and append it to the doc
   const targetElementId = 'sequence-grid';
-
   const contextMenu = document.createElement('div');
   contextMenu.className = 'custom-context-menu';
   contextMenu.innerHTML = `
     <ul>
       <li id="insertion">Insert here</li>
       <li id="deletion" disabled>Delete selection</li>
-      <li id="mutation" disabled>Mutate selection</li>
+      <li id="mutation" disabled>Mutate/Replace selection</li>
       <li id="subcloning" disabled>Subclone selection</li>
       <li id="begin-translation" disabled>Begin translation here</li>
     </ul>
   `;
   document.body.appendChild(contextMenu);
 
-  function handleContextMenu(clientX, clientY) {
-    const table = document.getElementById(targetElementId);
-  
-    if (selectedText) {
-      console.log('Selected text:', selectedText);
-    }
-    insertionPosition = basePosition;
-    console.log('Clicked position:', insertionPosition);
-  
-    updateMenuItems();
-  
-    positionContextMenu(clientX, clientY);
-  }
-  
-
-  function hideContextMenu() {
-    const contextMenu = document.querySelector('.custom-context-menu');
-    contextMenu.style.display = 'none';
-  }
-
-  document.addEventListener('contextmenu', function (event) {
-    event.preventDefault();
-
-    if (event.target.matches(`#${targetElementId} td`)) {
-      handleContextMenu(event.clientX, event.clientY);
-    } else {
-      hideContextMenu();
-    }
+  // Right click context menu logic while clicking on the content grid
+  document.getElementById("content").addEventListener('contextmenu', function (event) {
+    event.preventDefault(); // Prevent default right click menu
+    handleContextMenu(event.clientX, event.clientY);
   });
 
+  // Hite the context menu when clicking outside it
   document.addEventListener('click', function (event) {
     if (!event.target.closest('.custom-context-menu')) {
       hideContextMenu();
     }
   });
 
+  /**
+   * Menu items listeners
+   */
   contextMenu.addEventListener('click', function (event) {
     event.stopPropagation();
+
+    // Select clicked menu item if it is enabled
     const menuItemId = event.target.id;
     const menuItem = document.getElementById(menuItemId);
     if (menuItem.disabled) {
       return;
     }
 
+    // Item logic
     if (menuItemId === 'insertion') {
       console.log('Insertion selected');
-
-      // Show the popup window for sequence insertion
-      showPopupWindow("Insert here:");
+      showPopupWindow("Insert here:"); // Show the popup window for insertions/replacements
     } else if (menuItemId === 'deletion') {
       console.log('Deletion selected');
-      createDeletionPrimers(selectionStartPos, selectionEndPos);
-      // Deletion logic here
+      createDeletionPrimers(selectionStartPos, selectionEndPos); // Create deletion primers
     } else if (menuItemId === 'mutation') {
       console.log('Mutation selected');
-      //showMutationPopupWindow();
-      showPopupWindow("Replace selection with:");
-      // Mutation logic here
+      showPopupWindow("Replace selection with:"); // Show the popup window for insertions/replacements
     } else if (menuItemId === 'subcloning') {
       console.log('Subcloning selected');
-      // Subcloning logic here
-      createSubcloningPrimers(selectionStartPos, selectionEndPos);
+      createSubcloningPrimers(selectionStartPos, selectionEndPos); // Start subcloning logic
     } else if (menuItemId === 'begin-translation') {
       console.log('Beginning translation');
-      if (sequence.slice(insertionPosition - 1, insertionPosition + 2) === "ATG") {
+      // Start translation logic
+      if (sequence.slice(insertionPosition - 1, insertionPosition + 2) === "ATG") { // If we clicked on ATG, start translation there
         startTranslation(insertionPosition, 1);
-      } else {
+      } else { // Else search for the first ATG then start there
         startTranslation(sequence.indexOf("ATG", insertionPosition) + 1, 1);
       }
     }
 
+    // Hide the menu once done
     hideContextMenu();
   });
 
-  function getCharacterOffset(target) {
-    const tableRow = target.parentNode;
-    const cellIndex = Array.from(tableRow.cells).indexOf(target);
 
-    return { rowIndex: tableRow.rowIndex, cellIndex };
+  /**
+   * 
+   */
+  function handleContextMenu(clientX, clientY) {
+    // Record cursor position
+    insertionPosition = basePosition;
+  
+    // Select all the menu items
+    const insertionMenuItem = document.getElementById('insertion');
+    const deletionMenuItem = document.getElementById('deletion');
+    const mutationMenuItem = document.getElementById('mutation');
+    const subcloningMenuItem = document.getElementById('subcloning');
+    const beginTranslationMenuItem = document.getElementById('begin-translation');
+
+    // Enable or disable menu items based on if the user is making a selection
+    if (selectedText) {
+      // If there is a selection, disable insertions and translations
+      insertionMenuItem.classList.add('disabled');
+      beginTranslationMenuItem.classList.add('disabled');
+      // Re-enable deletions and mutations
+      deletionMenuItem.classList.remove('disabled');
+      mutationMenuItem.classList.remove('disabled');
+    } else {
+      // If there is no selection, re-enable insertions and translations
+      insertionMenuItem.classList.remove('disabled');
+      beginTranslationMenuItem.classList.remove('disabled');
+      // Disable deletions and mutations
+      deletionMenuItem.classList.add('disabled');
+      mutationMenuItem.classList.add('disabled');
+    }
+
+    // If there is a selection and a second plasmid has been imported, enable the subcloning option
+    if (selectedText && secondPlasmidIported) {
+      subcloningMenuItem.classList.remove('disabled');
+    } else {
+      subcloningMenuItem.classList.add('disabled');
+    }
+    
+    // Reposition the context menu
+    positionContextMenu(clientX, clientY);
+  }
+  
+
+  /**
+   * Hides the context menu.
+   */
+  function hideContextMenu() {
+    const contextMenu = document.querySelector('.custom-context-menu');
+    contextMenu.style.display = 'none';
   }
 });
