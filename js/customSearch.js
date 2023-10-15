ea89284d55e39c2ab6d3e48c2063c2063ab9061e
RaduLeonte <raducristian.leonte@stud.unibas.ch>
2023-10-15 22:57:38 +0200

    added search functionality to the complementary strand

--- js/customSearch.js
+++ js/customSearch.js
@@ -95,48 +95,63 @@ function initiateSearchFunctionality(pNr) {
 
         // Get search query from the search bar
         const searchQuery = customSearchInput.value;
+        const searchQueryComplement = searchQuery.split('').reverse().join('');;
+        console.log("Search:", searchQuery, searchQueryComplement)
 
         // Select the sequence and grid structure of the plasmid of interest
         let currSequence = null;
+        let currSequenceComp = null;
         let currGridStructure = null;
         if (pNr === 1) {
             currSequence = sequence;
+            currSequenceComp = complementaryStrand;
             currGridStructure = gridStructure;
         } else {
             currSequence = sequence2;
+            currSequenceComp = complementaryStrand2;
             currGridStructure = gridStructure2;
         }
 
         // If the query is not empty
         if (searchQuery) {
-            console.log(searchQuery)
-
-            // Get a list of indices for all occurences of the search query
-            const indices = [];
-            let currentIndex = currSequence.indexOf(searchQuery);
-            while (currentIndex !== -1) {
-                indices.push(currentIndex);
-                currentIndex = currSequence.indexOf(searchQuery, currentIndex + 1);
-            }
-
-            // Select table element
-            let table = null;
-            if (pNr === 1) {
-                table = document.getElementById("sequence-grid");
-            } else {
-                table = document.getElementById("sequence-grid2");
-            }
-            
-            // Iterate over all cells that contain the search query and highlight them
-            for (const index of indices) {
-                for (let i = 0; i < searchQuery.length; i++) {
-                    // Convert sequence index to table coordinates
-                    const [row, column] = seqIndexToCoords(index + i, 0, currGridStructure);
-                    // Select and highlight the cell
-                    const cell = table.rows[row].cells[column + 1];
-                    cell.classList.add("selected-cell-search");
+            for (i = 0; i < 2; i++) {
+                let workingSequence = "";
+                let workingQuery = "";
+                if (i === 0) {
+                    workingSequence = currSequence;
+                    workingQuery = searchQuery;
+                } else {
+                    workingSequence = currSequenceComp;
+                    workingQuery = searchQueryComplement;
                 }
+                // Get a list of indices for all occurences of the search query
+                const indices = [];
+                let currentIndex = workingSequence.indexOf(workingQuery);
+                while (currentIndex !== -1) {
+                    indices.push(currentIndex);
+                    currentIndex = workingSequence.indexOf(workingQuery, currentIndex + 1);
+                }
+
+                // Select table element
+                let table = null;
+                if (pNr === 1) {
+                    table = document.getElementById("sequence-grid");
+                } else {
+                    table = document.getElementById("sequence-grid2");
+                }
+                
+                // Iterate over all cells that contain the search query and highlight them
+                for (const index of indices) {
+                    for (let j = 0; j < workingQuery.length; j++) {
+                        // Convert sequence index to table coordinates
+                        const [row, column] = seqIndexToCoords(index + j, 0, currGridStructure);
+                        // Select and highlight the cell
+                        const cell = table.rows[row + i].cells[column + 1];
+                        cell.classList.add("selected-cell-search");
+                    }
             }
+            };
+
             // Scroll to the nearest occurence of the search query
             scrollToNextSelectedCell(pNr);
         }
