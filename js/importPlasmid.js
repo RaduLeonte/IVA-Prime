32f854459341249624736b5a51998c4bec4dbf79
RaduLeonte <raducristian.leonte@stud.unibas.ch>
2023-10-20 19:51:47 +0200

    annotation triangles fully implemented

--- js/importPlasmid.js
+++ js/importPlasmid.js
@@ -927,10 +927,19 @@ function makeContentGrid(pNr, callback) {
         cell.id = currGridStructure[i % currGridStructureLength];
         // Add a cell class
         cell.classList.add(currGridStructure[i % currGridStructureLength].replace(" ", ""));
+        if (cell.id === "Forward Strand" && currentChar !== "") {
+          cell.classList.add("forward-strand-base");
+        };
+
         // Append the cell to the row
         row.appendChild(cell);
       };
+
     };
+
+    // Get cell width in current window for annotation triangles
+    window.addEventListener('resize', updateAnnotationTrianglesWidth);
+    updateAnnotationTrianglesWidth();
     
     // Iterate over the features and create the annotatations
     //console.log("Here6", currFeatures)
@@ -1588,15 +1597,43 @@ function createFilledTriangle(featureID, triangleColor, orientation, row, col) {
   triangle.style.borderTop = "var(--triangle-height) solid transparent";
   triangle.style.borderBottom = "var(--triangle-height) solid transparent";
   if (orientation === "right") {
+    const styleElement = document.createElement('style');
+    const dynamicCSS = `
+      .${featureID + "-cell-right"} {
+        border-right: 2px solid var(--${triangleColorVariable});
+      }
+    `;
+    styleElement.textContent = dynamicCSS;
+    document.head.appendChild(styleElement);
+    cell.classList.add(featureID + "-cell-right");
+
     triangle.style.borderLeft = `var(--triangle-width) var(--${triangleColorVariable}) solid`;
   } else {
+    const styleElement = document.createElement('style');
+    const dynamicCSS = `
+      .${featureID + "-cell-left"} {
+        border-left: 2px solid var(--${triangleColorVariable});
+      }
+    `;
+    styleElement.textContent = dynamicCSS;
+    document.head.appendChild(styleElement);
+    cell.classList.add(featureID + "-cell-left");
+
     triangle.style.borderRight = `var(--triangle-width) var(--${triangleColorVariable}) solid`;
     triangle.style.position = "absolute";
-    triangle.style.right = "-1px";
+    triangle.style.right = "0px";
     triangle.style.top = "0px";
   };
 
-
   // Add the triangle div to the table cell
   cell.appendChild(triangle);
+};
+
+
+/**
+ * Update the global variables controlling the width of annotation triangles
+ */
+function updateAnnotationTrianglesWidth() {
+  const randomCell = document.getElementById("Forward Strand");
+  document.documentElement.style.setProperty('--triangle-width', randomCell.offsetWidth + 'px');
 };
