2a6c86829d78ca510a8df38eecf3ca8ab0de55e8
RaduLeonte <raducristian.leonte@stud.unibas.ch>
2024-01-07 23:48:18 +0100

    unprotected the microsynth xlsx file

--- static/js/createPrimers.js
+++ static/js/createPrimers.js
@@ -232,7 +232,7 @@ const exportPrimersDict = {
             })
             .then((workbook) => {
                 const worksheet = workbook.getWorksheet("DNA Order"); // Assuming the second sheet needs to be modified
-                //worksheet.unprotect();
+                worksheet.unprotect();
                 console.log("Microsynth", worksheet.name, primerList);
 
                 for (let i = 0; i < primerList.length; i++) {
