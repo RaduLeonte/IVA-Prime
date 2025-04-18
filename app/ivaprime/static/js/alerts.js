f00f00b468ec97a8dd57571d0d7872f3ca4a0fc6
RaduLeonte <raducristian.leonte@gmail.com>
2025-04-18 12:18:24 +0200

    cleaned up alerts.js

--- app/ivaprime/static/js/alerts.js
+++ app/ivaprime/static/js/alerts.js
@@ -1,9 +1,30 @@
 const Alerts = new class {
     constructor () {
-        // Show cookie info alert if the user has not previously dismissed it
+        // Alerts container that alerts are appended to
+        this.alertsContainer;
+
         document.addEventListener("DOMContentLoaded", () => {
+            // Store reference to alerts container
+            Alerts.alertsContainer = document.getElementById("alerts-container");
+
+            // Show cookie info alert if the user has not previously dismissed it
             if (!UserPreferences.get("cookieConsentGiven")) Alerts.showCookieConsentDisclaimer();
         });
+    };
+
+
+    /**
+     * Append alert as first child of the alerts container.
+     * 
+     * @param {HTMLElement} alert - Reference to alert to append
+     */
+    _appendAlert(alert) {
+        if (!this.alertsContainer) {
+            console.warn("Alerts container not found.")
+            return;
+        };
+
+        this.alertsContainer.insertBefore(alert, this.alertsContainer.firstChild);
     };
 
 
@@ -16,12 +37,7 @@ const Alerts = new class {
      * @param {String} color - CSS color for left border and svg icon
      */
     warning(title, body, lifetime=5, color="orange", ) {
-        this.showAlert(
-            title,
-            body,
-            lifetime,
-            color,
-        );
+        this.showAlert(title, body, lifetime, color);
     };
 
 
@@ -34,12 +50,7 @@ const Alerts = new class {
      * @param {String} color - CSS color for left border and svg icon
      */
     error(title, body, lifetime=-1, color="red") {
-        this.showAlert(
-            title,
-            body,
-            lifetime,
-            color,
-        );
+        this.showAlert(title, body, lifetime, color);
     };
 
 
@@ -52,62 +63,59 @@ const Alerts = new class {
      * @param {String} color - CSS color for left border and svg icon
      */
     showAlert(title, body, lifetime=5, color="orange", ) {
-        // Select alert parent container
-        const alertsContainer = document.getElementById("alerts-container");
-        
         // Create new alert element
         const alert = document.createElement('div');
         alert.classList.add("alert");
         alert.style.borderColor = color;
 
+
         // Alert icon
         const alertIconContainer = document.createElement("DIV");
         alertIconContainer.classList.add("alert-icon");
-        alert.appendChild(alertIconContainer);
-        const alertIcon = document.createElement('svg');
-        alertIcon.innerHTML = `
+        alert.appendChild(alertIconContainer); // Append
+        
+        const alertIconSvgWrapper = document.createElement('div');
+        alertIconSvgWrapper.classList.add("alert-icon-svg-wrapper");
+        alertIconSvgWrapper.innerHTML = `
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M9.82664 2.22902C10.7938 0.590326 13.2063 0.590325 14.1735 2.22902L23.6599 18.3024C24.6578 19.9933 23.3638 22 21.4865 22H2.51362C0.63634 22 -0.657696 19.9933 0.340215 18.3024L9.82664 2.22902ZM10.0586 7.05547C10.0268 6.48227 10.483 6 11.0571 6H12.9429C13.517 6 13.9732 6.48227 13.9414 7.05547L13.5525 14.0555C13.523 14.5854 13.0847 15 12.554 15H11.446C10.9153 15 10.477 14.5854 10.4475 14.0555L10.0586 7.05547ZM14 18C14 19.1046 13.1046 20 12 20C10.8954 20 10 19.1046 10 18C10 16.8954 10.8954 16 12 16C13.1046 16 14 16.8954 14 18Z" fill="${color}"/>
             </svg>
         `;
-        alertIconContainer.appendChild(alertIcon);
+        alertIconContainer.appendChild(alertIconSvgWrapper); // Append
+
 
         // Alert main body for title and text
         const alertBody = document.createElement('div');
-        alertBody.classList.add("alert-body")
-        
+        alertBody.classList.add("alert-body");
+        alert.appendChild(alertBody); // Append
+
         // Title
         const alertTitle = document.createElement('h4');
         alertTitle.innerHTML = title;
-        
+        alertBody.append(alertTitle); // Append
+
         // Alert text
         const alertText = document.createElement("div");
         alertText.classList.add("alert-text")
         alertText.innerHTML = body;
+        alertBody.append(alertText); // Append
         
         
-        alertBody.append(alertTitle)
-        alertBody.append(alertText)
-        alert.appendChild(alertBody);
-
         // Close button
         const closeButton = document.createElement('span');
         closeButton.classList.add("alert-close-button")
         closeButton.textContent = '×';
-        closeButton.addEventListener('click', function (e) {
-            Alerts.removeAlert(alert)
-        });
-        alert.appendChild(closeButton);
+        closeButton.addEventListener('click', function (e) { Alerts._removeAlert(alert) });
+        alert.appendChild(closeButton); // Append
+
 
         // Insert new alert as first child
-        alertsContainer.insertBefore(alert, alertsContainer.firstChild);
+        this._appendAlert(alert);
 
 
         // If the alert has a finite lifetime, remove it after the amount specified
         if (lifetime !== -1) {
-            setTimeout(function (e) {
-                Alerts.removeAlert(alert)
-            }, lifetime*1000);
+            setTimeout(function (e) { Alerts._removeAlert(alert) }, lifetime*1000);
         };
     };
 
@@ -115,12 +123,15 @@ const Alerts = new class {
     /**
      * Remove alert after playing animation
      * 
-     * @param {HTMLElement} alert - Alert HTML element
+     * @param {HTMLElement} alert - Alert reference
      * @param {number} animationDuration - Duration of fade out animation in s
      */
-    removeAlert(alert, animationDuration=0.5) {
-        // Create temporary css class to set the height of the alert
-        // to its current height
+    _removeAlert(alert, animationDuration=0.5) {
+        /**
+         * Create a temporary css class to set the explicit height of the alert
+         * to its dynamic height. This is require as css animations cannot
+         * transition from height: auto to height: 0 in every browser.
+         */
         const currentHeight = alert.offsetHeight;
         const tempClass = 'alter-temp-fixed-height-' + Utilities.newUUID();
         const styleEl = document.createElement('style');
@@ -129,7 +140,7 @@ const Alerts = new class {
         alert.classList.add(tempClass);
         alert.offsetHeight; // Force update the height by reading it
 
-        // Add class
+        // Add attribute to collapse alert
         alert.setAttribute("collapsed", "")
         
         // Remove alert and temporary css class after animation is done
@@ -141,12 +152,9 @@ const Alerts = new class {
 
 
     /**
-     * Show the cookie info alert
+     * Show the custom cookie info alert
      */
     showCookieConsentDisclaimer() {
-        // Select alert parent container
-        const alertsContainer = document.getElementById("alerts-container");
-        
         // Create new alert element
         const alert = document.createElement('div');
         alert.classList.add("alert");
@@ -157,10 +165,8 @@ const Alerts = new class {
         const alertBody = document.createElement('div');
         alertBody.classList.add("alert-body");
         alertBody.classList.add("cookie-consent-body");
+        alert.appendChild(alertBody); // Append
         
-        // Title
-        const alertTitle = document.createElement('h4');
-        alertTitle.innerHTML = "Cookie disclaimer";
         
         // Alert text
         const alertText = document.createElement("div");
@@ -172,6 +178,7 @@ const Alerts = new class {
         <p>
             <a href="/about#privacy-policy" target="_blank" class="underlined-link">Click here for more details</a>
         </p>`;
+        alertBody.append(alertText); // Append
         
         
         // Close button
@@ -182,19 +189,16 @@ const Alerts = new class {
             "cookie-consent-agree-button"
         );
         agreeButton.textContent = "Understood";
+
         agreeButton.addEventListener('click', function (e) {
             UserPreferences.set("cookieConsentGiven", true);
-            Alerts.removeAlert(alert);
+            Alerts._removeAlert(alert);
         });
-        
-        
-        // Construct element
-        alertBody.append(alertText)
-        alertBody.appendChild(agreeButton);
-        alert.appendChild(alertBody);
 
+        alertBody.appendChild(agreeButton); // Append
+        
 
         // Add alert
-        alertsContainer.insertBefore(alert, alertsContainer.firstChild);
+        this._appendAlert(alert);
     };
 };
