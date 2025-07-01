01069548fba5e43531f7e900abdad15a5561b504
RaduLeonte <raducristian.leonte@unibas.ch>
2025-07-01 17:07:35 +0200

    more work on installer

--- tauri/iva-prime/src-tauri/src/lib.rs
+++ tauri/iva-prime/src-tauri/src/lib.rs
@@ -47,6 +47,29 @@ async fn open_about_window(app: tauri::AppHandle) {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
+            // Run on second instance of IVA Prime
+            // `args` command line arguments of second instance
+            // `app` tauri AppHandle.
+
+            if let Some(main_window) = app.get_webview_window("main") {
+                // Collect arguments
+                let file_args: Vec<String> = args.into_iter().skip(1).collect();
+
+                let js_files = prepare_js_files(file_args);
+                let js_call = match serde_json::to_string(&js_files) {
+                    Ok(json) => format!(r#"FileIO.importBase64Files({})"#, json),
+                    Err(e) => {
+                        eprintln!("Failed to serialize JS file list: {}", e);
+                        return;
+                    }
+                };
+
+                if let Err(err) = main_window.eval(&js_call) {
+                    eprintln!("Failed to execute JavaScript: {}", err);
+                }
+            }
+        }))
         .invoke_handler(tauri::generate_handler![open_about_window])
         .setup(|app| {
             let args: Vec<String> = std::env::args().collect();
