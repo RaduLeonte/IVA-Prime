95cdf06f29771b51150b071d8e974aab84baf6b9
RaduLeonte <raducristian.leonte@unibas.ch>
2025-07-03 17:39:56 +0200

    changed file association logic

--- tauri/iva-prime/src-tauri/src/lib.rs
+++ tauri/iva-prime/src-tauri/src/lib.rs
@@ -1,12 +1,12 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 use tauri::Manager;
-use tokio::time::sleep;
-use std::time::Duration;
 use std::fs;
 use std::path::PathBuf;
 use base64::{engine::general_purpose, Engine};
 use serde::Serialize;
+use url::Url;
+//use tauri::Listener;
 
 
 #[derive(Serialize)]
@@ -15,13 +15,12 @@ struct JsFile {
     content_base64: String,
 }
 
-fn prepare_js_files(paths: Vec<String>) -> Vec<JsFile> {
+fn prepare_js_files(paths: Vec<PathBuf>) -> Vec<JsFile> {
     paths
         .into_iter()
         .filter_map(|path| {
-            let path_buf = PathBuf::from(&path);
-            let name = path_buf.file_name()?.to_string_lossy().to_string();
-            let bytes = fs::read(&path_buf).ok()?;
+            let name = path.file_name()?.to_string_lossy().to_string();
+            let bytes = fs::read(&path).ok()?;
             let encoded = general_purpose::STANDARD.encode(&bytes);
             Some(JsFile {
                 name,
@@ -29,6 +28,39 @@ fn prepare_js_files(paths: Vec<String>) -> Vec<JsFile> {
             })
         })
         .collect()
+}
+
+
+
+fn handle_file_associations(app: tauri::AppHandle, files: Vec<PathBuf>) {
+    let js_files = prepare_js_files(files);
+    let js_call = match serde_json::to_string(&js_files) {
+        Ok(json) => format!(
+            r#"
+            if (document.readyState !== 'complete') {{
+                window.addEventListener('DOMContentLoaded', function() {{
+                    FileIO.importBase64Files({});
+                }});
+            }} else {{
+                FileIO.importBase64Files({});
+            }}
+            "#,
+            json,
+            json
+        ),
+        Err(e) => {
+            eprintln!("Failed to serialize JS file list: {}", e);
+            return;
+        }
+    };
+
+    if let Some(main_window) = app.get_webview_window("main") {
+        if let Err(err) = main_window.eval(&js_call) {
+            eprintln!("Failed to execute JavaScript: {}", err);
+        }
+    } else {
+        eprintln!("Main window not found.");
+    }
 }
 
 #[tauri::command]
@@ -48,86 +80,75 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
-            // Run on second instance of IVA Prime
-            // `args` command line arguments of second instance
-            // `app` tauri AppHandle.
-
-            if let Some(main_window) = app.get_webview_window("main") {
-                // Collect arguments
-                let file_args: Vec<String> = args.into_iter().skip(1).collect();
-
-                let js_files = prepare_js_files(file_args);
-                let js_call = match serde_json::to_string(&js_files) {
-                    Ok(json) => format!(r#"FileIO.importBase64Files({})"#, json),
-                    Err(e) => {
-                        eprintln!("Failed to serialize JS file list: {}", e);
-                        return;
+            // Collect arguments for second instance
+            let file_args: Vec<String> = args.into_iter().skip(1).collect();
+            let files = file_args
+                .into_iter()
+                .filter_map(|maybe_file| {
+                    if maybe_file.starts_with('-') {
+                        return None; // Skip flags
                     }
-                };
 
-                if let Err(err) = main_window.eval(&js_call) {
-                    eprintln!("Failed to execute JavaScript: {}", err);
-                }
-            }
+                    if let Ok(url) = Url::parse(&maybe_file) {
+                        if let Ok(path) = url.to_file_path() {
+                            return Some(path);
+                        }
+                    }
+
+                    Some(PathBuf::from(maybe_file))
+                })
+                .collect::<Vec<_>>();
+
+            handle_file_associations(app.clone(), files);
         }))
         .invoke_handler(tauri::generate_handler![open_about_window])
         .setup(|app| {
-            let args: Vec<String> = std::env::args().collect();
-            println!("Startup arguments: {:?}", args);
-
-            let file_args: Vec<String> = args.into_iter().skip(1).collect();
-
-            if !file_args.is_empty() {
-                let app_handle = app.app_handle().clone();
-                tauri::async_runtime::spawn(async move {
-                    sleep(Duration::from_millis(1000)).await;
-
-                    if let Some(main_window) = app_handle.get_webview_window("main") {
-                        let js_files = prepare_js_files(file_args);
-                        let js_call = match serde_json::to_string(&js_files) {
-                            Ok(json) => format!(r#"FileIO.importBase64Files({})"#, json),
-                            Err(e) => {
-                                eprintln!("Failed to serialize JS file list: {}", e);
-                                return;
-                            }
-                        };
-
-                        if let Err(err) = main_window.eval(&js_call) {
-                            eprintln!("Failed to execute JavaScript: {}", err);
-                        }
-                    } else {
-                        eprintln!("Main window not found.");
-                    }
-                });
-            }
-            
-            #[cfg(target_os = "macos")]
+            #[cfg(any(windows, target_os = "linux"))]
             {
-                let app_handle = app.app_handle();
-                app.listen("tauri://open-file", move |event| {
-                    if let Some(payload) = event.payload() {
-                        let path: String = payload.into();
-                        println!("Received open-file event: {:?}", path);
-                        
-                        if let Some(main_window) = app_handle.get_webview_window("main") {
-                            let js_files = prepare_js_files(vec![path]);
-                            let js_call = match serde_json::to_string(&js_files) {
-                                Ok(json) => format!(r#"FileIO.importBase64Files({})"#, json),
-                                Err(e) => {
-                                    eprintln!("Failed to serialize JS file list: {}", e);
-                                    return;
-                                }
-                            };
-                            if let Err(err) = main_window.eval(&js_call) {
-                                eprintln!("Failed to execute JavaScript: {}", err);
+                let args: Vec<String> = std::env::args().collect();
+                println!("Startup arguments: {:?}", args);
+    
+                let file_args: Vec<String> = args.into_iter().skip(1).collect();
+                let files = file_args
+                    .into_iter()
+                    .filter_map(|maybe_file| {
+                        if maybe_file.starts_with('-') {
+                            return None; // Skip flags
+                        }
+    
+                        if let Ok(url) = Url::parse(&maybe_file) {
+                            if let Ok(path) = url.to_file_path() {
+                                return Some(path);
                             }
                         }
-                    }
-                });
+    
+                        Some(PathBuf::from(maybe_file))
+                    })
+                    .collect::<Vec<_>>();
+    
+                // Wait for the front-end to be loaded
+                if let Some(_main_window) = app.get_webview_window("main") {
+                    // Listen for the 'tauri://window-loaded' event
+                    let app_handle = app.app_handle();
+                    handle_file_associations(app_handle.clone(), files.clone());
+                }
             }
 
             Ok(())
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(
+            #[allow(unused_variables)]
+            |app, event| {
+            #[cfg(any(target_os = "macos"))]
+            if let tauri::RunEvent::Opened { urls } = event {
+                let files = urls
+                    .into_iter()
+                    .filter_map(|url| url.to_file_path().ok())
+                    .collect::<Vec<_>>();
+
+                handle_file_associations(app.app_handle().clone(), files.clone())
+            }
+        });
 }
